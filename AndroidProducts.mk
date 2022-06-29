d5465a252a330a379c8797f30cb91f182d9384e7
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-06-29 11:22:44 +0200

    picasso: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: xiaomi Model: Redmi K30 5G Script version: 2.3.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_picasso.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_picasso-user \
+    omni_picasso-userdebug \
+    omni_picasso-eng
